82ca224e57f73323bc5d2d2cf7fb14c773a5695d
amir aghighi <aghighiamir@yahoo.com>
2023-09-15 16:05:29 +0330

    add: add set builder

--- classes/ORMFacade.php
+++ classes/ORMFacade.php
@@ -3,11 +3,28 @@
 namespace classes;
 class ORMFacade
 {
+    private static ?SqlQueryBuilder $builder = null;
+
+    private static function setBuilder()
+    {
+
+        if (is_null(self::$builder)) {
+            $config = [
+                'dsn' => 'mysql:host=localhost;dbname=orm_facade',
+                'user' => 'root',
+                'password' => ''
+            ];
+            $pdo = new PDO($config['dsn'],$config['user'],$config['password']);
+            self::$builder = new SqlQueryBuilder($pdo);
+        }
+    }
+
     public static function __callStatic(string $name, array $arguments)
     {
-        $arr = preg_split('/(?=[A-Z])/',$name);
+
+        $arr = preg_split('/(?=[A-Z])/', $name);
 
         $crud = $arr[0];
-        $table = strtolower($arr[1])."s";
+        $table = strtolower($arr[1]) . "s";
     }
 }
